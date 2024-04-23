e4b87a267d88ea5e77a43978ffd22d0f3eb849b7
SnigdhaSingh01 <100959301+SnigdhaSingh01@users.noreply.github.com>
2024-04-23 14:54:06 +0530

    Create merge_two_array.rs
    11.Code to merge  two sorte arrays

---
+++ merge_two_array.rs
@@ -0,0 +1,36 @@
+//11.Merge two sorted arrays in Rust
+
+fn merge_sorted_arrays(arr1: &[i32], arr2: &[i32]) -> Vec<i32> {
+    let mut merged = Vec::new();
+    let (mut i, mut j) = (0, 0);
+
+    while i < arr1.len() && j < arr2.len() {
+        if arr1[i] < arr2[j] {
+            merged.push(arr1[i]);
+            i += 1;
+        } else {
+            merged.push(arr2[j]);
+            j += 1;
+        }
+    }
+
+    while i < arr1.len() {
+        merged.push(arr1[i]);
+        i += 1;
+    }
+
+    while j < arr2.len() {
+        merged.push(arr2[j]);
+        j += 1;
+    }
+
+    merged
+}
+
+fn main() {
+    let arr1 = [1, 3, 5, 7, 9];
+    let arr2 = [2, 4, 6, 8, 10];
+
+    let merged = merge_sorted_arrays(&arr1, &arr2);
+    println!("Merged array: {:?}", merged);
+}

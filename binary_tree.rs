50c681e2a81e1219b58e5306c58c172641a80ace
SnigdhaSingh01 <100959301+SnigdhaSingh01@users.noreply.github.com>
2024-04-23 14:41:00 +0530

    Update binary_tree.rs
    8.Binary tree implementation

--- binary_tree.rs
+++ binary_tree.rs
@@ -1,4 +1,4 @@
-8.Given a binary tree, implement a function that returns the maximum depth of the tree.
+//8.Given a binary tree, implement a function that returns the maximum depth of the tree.
 
 use std::cmp;
 
